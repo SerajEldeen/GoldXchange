074607c1ec5dd8167202c866f8059a2cabdb50cb
Seraj Elseen Abdullah <serageldeen13@gmail.com>
2025-02-14 09:27:19 +0200

    fix:verification issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return null;
+  return (
+    <>
+      <footer className="mt-8 flex justify-center items-center w-full fixed bottom-0 bg-tertiary p-4">
+        <a href="/privacy-policy" className="text-primary underline">
+          Privacy Policy
+        </a>
+      </footer>
+    </>
+  );
 }
