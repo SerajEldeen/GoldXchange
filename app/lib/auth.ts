2d87b20d9507e21c1f432d558db4e4948349b348
Seraj Elseen Abdullah <serageldeen13@gmail.com>
2025-02-14 09:06:46 +0200

    fix:auth.ts

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-export const { auth, handlers, signIn, signOut } = NextAuth({
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
